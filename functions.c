972d7c3af9d9299142b55e3ba3d2a757445853bf
Amah-amara <99175765+Amah-amara@users.noreply.github.com>
2023-01-23 21:57:28 +0100

    Update functions.c

--- functions.c
+++ functions.c
@@ -95,6 +95,7 @@ int print_percent(va_list types, char buffer[],
 	UNUSED(size);
 	return (write(1, "%%", 1));
 }
+/**
  ************************ PRINT INT **************************/
 /**
  * print_int - Print int
