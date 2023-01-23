9b1e860ad57df79e75f87c692297292daa78d0aa
Amah-amara <cynama99@gmail.com>
2023-01-23 13:21:02 -0800

    update

--- printf.c
+++ printf.c
@@ -1,6 +1,5 @@
 #include "main.h"
 
-<<<<<<< HEAD
 void print_buffer(char buffer[], int *buff_ind);
 
 /**
@@ -65,56 +64,3 @@ void print_buffer(char buffer[], int *buff_ind)
 
 	*buff_ind = 0;
 }
-
-
-=======
-
-
-void print_buffer(char buffer[], int *buff_ind);
-
-
-
-/**
-
- * _printf - Printf function
-
- * @format: format.
-
- * Return: Printed chars.
-
- */
-
-int _printf(const char *format, ...)
-
-{
-
-	int i, printed = 0, printed_chars = 0;
-
-	int flags, width, precision, size, buff_ind = 0;
-
-	va_list list;
-
-	char buffer[BUFF_SIZE];
-
-
-
-	if (format == NULL)
-
-		return (-1);
-
-
-
-	va_start(list, format);
-
-
-
-	for (i = 0; format && format[i] != '\0'; i++)
-
-	{
-
-		if (format[i] != '%')
-
-		{
-
-			buffer[buff_ind++] = format[i];
->>>>>>> b5efd70a78ad515f321bbe93724035a10e8585b6
